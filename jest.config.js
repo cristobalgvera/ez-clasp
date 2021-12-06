9a28a38cc363b33c00d1a066514d6ee4c965512f
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-12-06 17:36:08 -0300

    :wrench: Updated Jest coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
