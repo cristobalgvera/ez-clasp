f64a170c6aa3e8ab9b9087c0527d24ba9c40e0c6
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-12-06 22:41:59 -0300

    :hammer: Updated TypeScript check script
    Also was updated app entry point to ensure proper usage of TypeScript configuration file.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import ByeService from './services/bye.service';
 import HelloService from './services/hello.service';
 
+// @ts-ignore
 function main(): void {
   const hiMessage = HelloService.sayHi('CLASP');
   const byeMessage = ByeService.sayBye('CLASP');
 
-  console.log(hiMessage);
-  console.log(byeMessage);
+  console.log({ hiMessage, byeMessage });
 }
