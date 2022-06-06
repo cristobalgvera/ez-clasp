2a3ac0e8d11c2bcdda52a274e2837fb049678668
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-06-06 02:03:13 -0400

    test(config): added test match string to typescript files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  testMatch: ['**/?(*.)+(spec|test).ts'],
   preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
