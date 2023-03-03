5d119b17b0c8e6ecacb2a54d2d1b35395e14e26f
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-03 17:43:22 -0300

    chore(eslint): improve check on entry point file

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,21 @@ const eslintConfig = {
         project: ['./tsconfig.json'],
       },
     },
+    {
+      parser: '@typescript-eslint/parser',
+      files: ['src/index.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
   ],
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import ByeService from './services/bye.service';
 import HelloService from './services/hello.service';
 
-export function main(): void {
+// @ts-ignore
+function main(): void {
   const hiMessage = HelloService.sayHi('CLASP');
   const byeMessage = ByeService.sayBye('CLASP');
 
